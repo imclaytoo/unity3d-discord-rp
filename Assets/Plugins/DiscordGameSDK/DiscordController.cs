53c20a288ca9a0ed64a44c2fbae60b2aca70958c
Murran <claytoo@sfcerenity.org>
2023-01-12 11:59:36 +0700

    Update: includes Apple silicon (aarch64) support
    - Includes Apple silicon (aarch64) support.

--- Assets/Plugins/DiscordGameSDK/DiscordController.cs
+++ Assets/Plugins/DiscordGameSDK/DiscordController.cs
@@ -1,40 +1,98 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Discord;
+using UnityEngine;
 
 public class DiscordController : MonoBehaviour
 {
+    public long applicationID;
+    [Space]
+    public string details = "In Game";
+    public string state = "Solving Puzzle";
+    [Space]
+    public string largeImage = "game_logo";
+    public string largeText = "big";
+    [Space]
+    public string smallImage = "company_logo";
+    public string smallText = "small";
+
+    private long time;
+
+    private static bool instanceExists;
     public Discord.Discord discord;
 
-    public string sDetails, sState, sLargeImage, sLargeText, sSmallImage, sSmallText;
+    void Awake()
+    {
+        // Transition the GameObject between scenes, destroy any duplicates
+        if (!instanceExists)
+        {
+            instanceExists = true;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
-        discord = new Discord.Discord(/*PUT_YOUR_CLIENT ID_HERE*/, (System.UInt64)Discord.CreateFlags.Default);
-        var activityManager = discord.GetActivityManager();
-        var activity = new Discord.Activity
-        {
-            Details = sDetails,
-            State = sState,
-            Assets = 
-            {
-                LargeImage = sLargeImage,
-                LargeText = sLargeText,
-                SmallImage = sSmallImage,
-                SmallText = sSmallText
-            },
-        };
-        activityManager.UpdateActivity(activity, (res) => {
-            if (res == Discord.Result.Ok)
-                Debug.Log("Discord status set!");
-            else
-                Debug.LogError("Discord status failed!");
-        });
+        // Log in with the Application ID
+        discord = new Discord.Discord(applicationID, (System.UInt64)Discord.CreateFlags.NoRequireDiscord);
+
+        time = System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+        UpdateStatus();
     }
 
     void Update()
     {
-        discord.RunCallbacks();
+        // Destroy the GameObject if Discord isn't running
+        try
+        {
+            discord.RunCallbacks();
+        }
+        catch
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void LateUpdate()
+    {
+        UpdateStatus();
+    }
+
+    void UpdateStatus()
+    {
+        // Update Status every frame
+        try
+        {
+            var activityManager = discord.GetActivityManager();
+            var activity = new Discord.Activity
+            {
+                Details = details,
+                State = state,
+                Assets =
+                {
+                    LargeImage = largeImage,
+                    LargeText = largeText,
+                    SmallImage = smallImage,
+                    SmallText = smallText
+                },
+                Timestamps =
+                {
+                    Start = time
+                }
+            };
+
+            activityManager.UpdateActivity(activity, (res) =>
+            {
+                if (res != Discord.Result.Ok) Debug.LogWarning("Failed connecting to Discord!");
+            });
+        }
+        catch
+        {
+            // If updating the status fails, Destroy the GameObject
+            Destroy(gameObject);
+        }
     }
 }
